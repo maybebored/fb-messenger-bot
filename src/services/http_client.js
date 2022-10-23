81953c13baa69ae9dd304442f6e2d4edaa312d96
maybebored <mayuselvaraja@gmail.com>
2022-10-24 06:18:41 +0800

    add basic http client class

---
+++ src/services/http_client.js
@@ -0,0 +1,34 @@
+var httpClient = require('http-client');
+
+module.exports = class HttpClient {
+    constructor({ baseUrl }) {
+        this.baseUrl = baseUrl;
+    }
+
+    doGet(path, params) {
+        let client = httpClient.createFetch(
+            httpClient.base(this.baseUrl),
+            httpClient.accept('application/json'),
+            httpClient.parse('json'),
+            httpClient.method('GET'),
+            httpClient.params(params),
+        );
+        client(path).then(response => {
+            console.log(response.jsonData);
+        })
+    }
+
+    doPost(path, params, body) {
+        let client = httpClient.createFetch(
+            httpClient.base(this.baseUrl),
+            httpClient.accept('application/json'),
+            httpClient.parse('json'),
+            httpClient.method('POST'),
+            httpClient.params(params),
+            httpClient.body(JSON.stringify(body)),
+        );
+        client(path).then(response => {
+            console.log(response.jsonData);
+        })
+    }
+};
